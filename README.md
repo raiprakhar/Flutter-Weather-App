0461aec3cc986700e232050394c612c6a9a9e15a
raiprakhar <29.raiprakhar@gmail.com>
2020-07-31 12:57:47 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flutter Weather App
+
+## Functionalities
+
+  Find out the live weather data in the current location of the device as well as the weather for any city you can think of!
+
+![Finished App](https://github.com/raiprakhar/Flutter-Weather-App/Images/clima-demo.gif)
+
+
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,37 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:clima/services/weather.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  WeatherModel weatherModel = WeatherModel();
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(weatherData);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingCircle(
+          color: Colors.white10,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
